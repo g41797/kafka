3090de0612543c3aac253adc5d6c01b3dd3de830
g41797 <g41797@gmail.com>
2024-07-04 08:56:55 +0300

    Add rdkafka

--- docker/addrdkafka.sh
+++ docker/addrdkafka.sh
@@ -27,13 +27,14 @@ sudo $PKMAN -y update  \
 
 $PKMAN install python3-pip
 
-git clone --depth 1 https://github.com/edenhill/librdkafka.git \
+sudo git clone --depth 1 https://github.com/edenhill/librdkafka.git \
     && ( \
         cd librdkafka \
         && ./configure \
         && make \
         && make install \
     ) \
+    && $PKMAN install librdkafka-dev
     && pecl install rdkafka \
     && echo "extension=rdkafka.so" > /usr/local/etc/php/conf.d/rdkafka.ini
 
